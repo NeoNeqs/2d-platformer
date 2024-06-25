329e7c744279f71c834769e1475c88407a847d6d
Michael <76450252+NeoNeqs@users.noreply.github.com>
2024-06-25 17:42:13 +0200

    Add player controller

---
+++ player/Player.cs
@@ -0,0 +1,39 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody2D
+{
+	private const float Speed = 120.0f;
+	private const float JumpVelocity = -320.0f;
+
+	// Get the gravity from the project settings to be synced with RigidBody nodes.
+	private float _gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+			velocity.Y += _gravity * (float)delta;
+
+		// Handle Jump.
+		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+			velocity.Y = JumpVelocity;
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		float direction = Input.GetAxis("left", "right");
+		if (direction != 0)
+		{
+			velocity.X = direction * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
